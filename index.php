10a28b45381492728e041cb5644419c28ef26659
Jesus Valera <jesus1902@outlook.com>
2018-10-05 20:03:51 +0200

    View in a new template folder

--- index.php
+++ index.php
@@ -4,25 +4,14 @@
 $conn = mysqli_connect('localhost', 'root', 'toor', 'front_controller');
 
 $result = mysqli_query($conn, 'SELECT id, title FROM post');
-?>
 
-    <html>
-        <head>
-        <title>List of Posts</title>
-        </head>
-        <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = mysqli_fetch_assoc($result)): ?>
-            <li>
-                <a href="/show.php?id=<?php echo $row['id'] ?>">
-                    <?php echo $row['title'] ?>
-                    </a>
-            </li>
-            <?php endwhile; ?>
-        </ul>
-        </body>
-    </html>
+$posts = [];
 
-<?php
+while ($row = mysqli_fetch_assoc($result))
+{
+	$posts[] = $row;
+}
+
 mysqli_close($conn);
+
+require 'templates/list.php';
